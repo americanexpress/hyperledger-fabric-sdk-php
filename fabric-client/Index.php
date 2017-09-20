a3a49a24224d112a3ce9e1928dc9fc664a8dcdd2
Jitendra Singh Dikhit <Jitendra.S.Dikhit@aexp.com>
2017-09-20 14:47:02 +0530

    autoload check

--- fabric-client/Index.php
+++ fabric-client/Index.php
@@ -1,5 +1,13 @@
 <?php
-$loader = require __DIR__ . "/../vendor/autoload.php";
+
+$loader_path = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($loader_path)) {
+    echo "Dependencies must be installed using composer:\n\n";
+    echo "php composer.phar install\n\n";
+    echo "See http://getcomposer.org for help with installing composer\n";
+    exit(1);
+}
+$loader = include $loader_path;
 
 $dir = dirname(__FILE__).'/lib';
 
