06204bea8ec4b3ec3344cf9a799099e19f098fda
Aniket Kumar Agrawal <aniket.k.agrawal@aexp.com>
2017-09-21 15:59:58 +0530

    changes

--- test/integration/E2EUtils.php
+++ test/integration/E2EUtils.php
@@ -5,22 +5,20 @@ use fabric\sdk;
 class E2EUtils
 {
 
-    private static $chainCodeName =  "sparrow_txn_cc";
-    private static $chainCodePath =  "github.com/sparrow_txn";
-    private static $chainCodeVersion ="2";
+    private static $chainCodeName =  "mycc";
+    private static $chainCodePath =  "go/chaincode_example02";
+    private static $chainCodeVersion ="1";
     private static  $chainChannelId = "foo";
 
     public function queryChaincode($org, $version, $value, $t, $transientMap)
     {
         $utils = new fabric\sdk\Utils();
 
-//        $nounce = $utils::getNonce();
-
-        $connect = $utils->FabricConnect();
+        $connect = $utils->FabricConnect($org);
 
         $channel = new fabric\sdk\Channel();
 
-        $fabricProposal = $channel->queryByChainCode($connect, self::$chainChannelId,self::$chainCodeName,self::$chainCodePath,self::$chainCodeVersion);
+        $fabricProposal = $channel->queryByChainCode($org, $connect, self::$chainChannelId,self::$chainCodeName,self::$chainCodePath,self::$chainCodeVersion);
 
 
     }
