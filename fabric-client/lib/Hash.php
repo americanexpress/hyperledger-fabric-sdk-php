e6349c7f648c58092d2ce964bed5419f12c4fd90
Jitendra Singh Dikhit <Jitendra.S.Dikhit@aexp.com>
2017-09-21 15:17:17 +0530

    removing unnecessary functions

--- fabric-client/lib/Hash.php
+++ fabric-client/lib/Hash.php
@@ -9,45 +9,6 @@ use Mdanter\Ecc\Serializer\Signature\DerSignatureSerializer;
 class Hash
 {
 
-    function generateBytes($string)
-    {
-        if (strlen($string) > 0) {
-            $bytearray = array();
-            foreach (str_split($string) as $i => $char) {
-                $bytearray[] = $this->ordutf8($char);
-            }
-            $bytestring = implode("", $bytearray);
-            return $bytestring;
-        }
-    }
-
-    function ordutf8($string)
-    {
-        $offset = 0;
-        $code = ord(substr($string, $offset, 1));
-        if ($code >= 128) {        //otherwise 0xxxxxxx
-            if ($code < 224) {
-                $bytesnumber = 2;                //110xxxxx
-            } elseif ($code < 240) {
-                $bytesnumber = 3;        //1110xxxx
-            } elseif ($code < 248) {
-                $bytesnumber = 4;    //11110xxx
-            }
-            $codetemp = $code - 192 - ($bytesnumber > 2 ? 32 : 0) - ($bytesnumber > 3 ? 16 : 0);
-            for ($i = 2; $i <= $bytesnumber; $i++) {
-                $offset++;
-                $code2 = ord(substr($string, $offset, 1)) - 128;        //10xxxxxx
-                $codetemp = $codetemp * 64 + $code2;
-            }
-            $code = $codetemp;
-        }
-        $offset += 1;
-        if ($offset >= strlen($string)) {
-            $offset = -1;
-        }
-        return $code;
-    }
-
     function generateByteArray($string)
     {
         $bytearray = unpack('c*', $string);
