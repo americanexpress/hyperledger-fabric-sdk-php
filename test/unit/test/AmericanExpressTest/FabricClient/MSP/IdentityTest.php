5030b835c42d96f6aa4ead1a0fe73d3d99e7b905
Timothy Younger <timothy.younger@aexp.com>
2017-11-06 09:30:23 -0700

    refactor: adds Identity test. refactored source to inject root path dependency, and added vfs test lib to create mock files.

---
+++ test/unit/test/AmericanExpressTest/FabricClient/MSP/IdentityTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AmericanExpressTest\HyperledgerFabricClient\MSP;
+
+use AmericanExpress\HyperledgerFabricClient\MSP\Identity;
+use Hyperledger\Fabric\Protos\MSP\SerializedIdentity;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \AmericanExpress\HyperledgerFabricClient\MSP\Identity
+ */
+class IdentityTest extends TestCase
+{
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $files;
+
+    /**
+     * @var Identity
+     */
+    private $sut;
+
+    protected function setUp()
+    {
+        $this->files = vfsStream::setup('test');
+
+        $this->sut = new Identity($this->files->url());
+    }
+
+    public function testCreateSerializedIdentity()
+    {
+        $certs = vfsStream::newFile('foo');
+        $certs->setContent('FizBuz');
+        $this->files->addChild($certs);
+
+        $identity = $this->sut->createSerializedIdentity('/foo', 'bar');
+
+        self::assertInstanceOf(SerializedIdentity::class, $identity);
+        self::assertSame('FizBuz', $identity->getIdBytes());
+        self::assertSame('bar', $identity->getMspid());
+    }
+}
