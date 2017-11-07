6b75e5afdd916a9377864f35c98b77a302a78c8d
Timothy Younger <timothy.younger@aexp.com>
2017-11-07 09:11:50 -0700

    test: adds more util tests.

--- test/unit/test/AmericanExpressTest/FabricClient/Utils/UtilsTest.php
+++ test/unit/test/AmericanExpressTest/FabricClient/Utils/UtilsTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace AmericanExpressTest\FabricClient\Utils;
 
@@ -26,5 +27,46 @@ class UtilsTest extends TestCase
         $nonce = $this->sut->getNonce();
 
         self::assertSame(24, strlen($nonce));
+    }
+
+    public function testGetByteArray()
+    {
+        $actual = $this->sut->toByteArray('FooBar');
+
+        $expected = [
+            1 => 70,
+            2 => 111,
+            3 => 111,
+            4 => 66,
+            5 => 97,
+            6 => 114,
+        ];
+
+        self::assertSame($expected, $actual);
+    }
+
+    public function testFabricConnect()
+    {
+        self::markTestIncomplete('Inject config to make this function testable.');
+    }
+
+    public function testCreateChaincodeInvocationSpec()
+    {
+        $result = $this->sut->createChaincodeInvocationSpec([
+            'foo',
+            'bar',
+        ]);
+
+        self::assertSame(['foo', 'bar'], iterator_to_array($result->getChaincodeSpec()->getInput()->getArgs()));
+    }
+
+    public function testProposalArrayToBinaryString()
+    {
+        $result = $this->sut->proposalArrayToBinaryString([
+            'foo',
+            'bar',
+        ]);
+
+        self::assertNotEmpty($result);
     }
 }
